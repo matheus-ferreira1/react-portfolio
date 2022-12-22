4cf919fbe215c58745c25e8bd374884a1c90cfd6
Matheus Ferreira <matheus.fer33@gmail.com>
2022-12-21 21:53:17 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header>teste</header>
+      <main>tteste</main>
+      <footer>teste</footer>
+    </div>
+  );
+}
+
+export default App;
