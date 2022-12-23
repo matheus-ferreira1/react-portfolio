5d01b2fa76c33ffcf497722eac3f6ff6993cfe5f
Matheus Ferreira <matheus.fer33@gmail.com>
2022-12-22 22:13:05 -0300

    navbar component finalized

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div className="App">
-      <header>teste</header>
-      <main>tteste</main>
-      <footer>teste</footer>
+    <div className="w-full bg-black overflow-hidden">
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,17 @@
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+    path: "/",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+    path: "/projects",
+  },
+  {
+    id: "experience",
+    title: "Experience",
+    path: "/experience",
+  },
+];
