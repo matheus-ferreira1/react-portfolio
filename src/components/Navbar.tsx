77bbd04b42ae05ab123590415d235130997df704
Matheus Ferreira <matheus.fer33@gmail.com>
2023-09-11 22:48:58 -0300

    fix: navbar css correction

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,17 +9,23 @@ interface NavbarProps {
 
 const Navbar: FC<NavbarProps> = ({ toggleTheme, theme }) => {
   return (
-    <header className="sticky top-0 z-10 border-b border-gray-200 dark:border-gray-500 backdrop-filter backdrop-blur-lg text-slate-900 dark:text-gray-200 ">
+    <header
+      id="home"
+      className="sticky top-0 z-10 border-b border-gray-200 dark:border-gray-500 backdrop-filter backdrop-blur-lg text-gray-900 dark:text-gray-200 "
+    >
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex items-center justify-between h-24">
           <span className="hidden sm:block text-3xl font-bold">
             matheus.ferreira
           </span>
 
-          <nav className="flex items-center">
-            <ul className="flex items-center gap-3">
+          <nav className="flex items-center text-base font-semibold text-gray-500 dark:text-gray-400">
+            <ul className="flex items-center gap-3 ">
               {links.map((link) => (
-                <li key={link.name}>
+                <li
+                  key={link.name}
+                  className="hover:text-gray-900 dark:hover:text-gray-100 transition-all duration-400"
+                >
                   <a href={link.link}>{link.name}</a>
                 </li>
               ))}
