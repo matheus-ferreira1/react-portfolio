e4109235ddd26977bb03d835ea09032caf27083b
Matheus Ferreira <matheus.fer33@gmail.com>
2023-09-11 21:22:42 -0300

    feat: added darkmode functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react";
+
+type Theme = "light" | "dark";
+
 function App() {
+  const [theme, setTheme] = useState<Theme | null>(null);
+
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (theme === "light") {
+      document.documentElement.classList.remove("dark");
+    } else {
+      document.documentElement.classList.add("dark");
+    }
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
-    <>
-      <h1>teste</h1>
-    </>
+    <div className="h-screen bg-white dark:bg-black flex flex-col justify-center items-center transition-colors duration-1000">
+      <h1 className="text-3xl font-bold text-black dark:text-white">
+        Hello world!
+      </h1>
+      <button className="bg-blue-600 text-black" onClick={toggleTheme}>
+        toggle theme
+      </button>
+    </div>
   );
 }
 
